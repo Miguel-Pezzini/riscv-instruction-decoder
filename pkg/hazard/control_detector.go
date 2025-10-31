f9fedc64805e230d389c271fb3de4bc2a03a717e
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-10-31 19:53:38 -0300

    fix: Control hazard doesnt have a data hazard method

--- pkg/hazard/control_detector.go
+++ pkg/hazard/control_detector.go
@@ -4,43 +4,11 @@ import "riscv-instruction-encoder/pkg/isa"
 
 func HasControlHazard(currentInstruction isa.PipelineInstruction, executing []*isa.PipelineInstruction, forwarding bool) bool {
 	for _, prev := range executing {
-		if hasBranchRAWHazard(currentInstruction, *prev, forwarding) || hasUnresolvedBranchHazard(currentInstruction, *prev) {
+		if hasUnresolvedBranchHazard(currentInstruction, *prev) {
 			return true
 		}
 	}
 
-	return false
-}
-
-func hasBranchRAWHazard(currentInstruction isa.PipelineInstruction, previousInstruction isa.PipelineInstruction, forwarding bool) bool {
-	currMeta := currentInstruction.Instruction.GetMeta()
-
-	if currMeta.IsBranch {
-		return false
-	}
-
-	prevMeta := previousInstruction.Instruction.GetMeta()
-
-	if prevMeta.Rd == nil || previousInstruction.HasCompleted {
-		return false
-	}
-
-	for _, rs := range currMeta.Rs {
-		if rs == *prevMeta.Rd {
-			cyclesToConsume := int(currMeta.ConsumeStage) - currentInstruction.CurrentStage
-			if !forwarding {
-				cyclesToProduce := int(isa.WB) - previousInstruction.CurrentStage
-				if cyclesToProduce >= 0 && cyclesToConsume >= 0 && cyclesToProduce > cyclesToConsume {
-					return true
-				}
-			} else {
-				cyclesToProduce := int(prevMeta.ProduceStage) - previousInstruction.CurrentStage
-				if cyclesToProduce >= 0 && cyclesToConsume >= 0 && cyclesToProduce > cyclesToConsume {
-					return true
-				}
-			}
-		}
-	}
 	return false
 }
 
